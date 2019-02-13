30556a2142e408c6935a3397cbd5502e38e37782
PavelKogol <pavelkogol19@gmail.com>
2019-02-13 16:14:04 +0200

    first commit PT1

--- Population.cs
+++ Population.cs
@@ -6,8 +6,15 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            int year;
+            for (year = 1; ; year++)
+            {
+                initialPopulation = Convert.ToInt32((1 + percent / 100) * initialPopulation + visitors);
+                if (initialPopulation >= currentPopulation)
+                    break;
+            }
+            //throw new NotImplementedException();
+            return year;
         }
     }
 }
